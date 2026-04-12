f6631ea83d2c4c6ae9fea4cfa534373e081dd5b5
stallone <stallone.developer@gmail.com>
2026-04-11 21:55:35 -0300

    CHORE: Update github assets

--- old_project/src/logger.ts
+++
@@ -1,7 +0,0 @@
-import { getLogger } from "@logtape";
-
-/**
- * Logger central da biblioteca CalcAUD.
- * Utiliza o framework LogTape para telemetria estruturada e rastreável.
- */
-export const Logger = getLogger(["calcaud-nbr-a11y"]);
