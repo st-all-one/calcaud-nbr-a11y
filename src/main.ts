cb16ada25877167263ba8e86498931d172432528
Stallone L. S. <95231541+st-all-one@users.noreply.github.com>
2026-04-22 07:04:38 -0300

    Criar main.ts

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+/*
+Factory para criação de singletons, deve permitir validar rapidamente a integridade via método estático e reidratar o cálculo.
+
+transferir toda a lógica do setPolicy para este ponto
+*/
